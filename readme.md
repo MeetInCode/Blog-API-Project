6f327aaf18142dc00c01e795a2db73f180aaaee9
meet mehta <meets.studentlife@gmail.com>
2024-07-20 21:40:41 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,50 @@
+to run this 2 terminals
+nodemon solution.js
+nodemon server.js 
+then localhost:4000
+
+when i want to crate a new post the flow
+
+ 1. link to /new which makes makes a get request to /new 
+  <a id="newPostBtn" href="/new">New Post</a> 
+
+ 2. /new will render the modify.ejs page   
+    app.get("/new", (req, res) => {
+    res.render("modify.ejs", { heading: "New Post", submit: "Create Post" });
+    });
+
+ 3. when submit button clicked make post request to /api/posts
+    else {%> 
+        <form id="newPostForm" method="post" action="/api/posts"> //go to api/posts
+          <input type="text" name="title" placeholder="Title" required>
+          <textarea name="content" placeholder="Content" required rows="10"></textarea>
+          <input type="text" name="author" placeholder="Author" required>
+          <button class="full-width" type="submit">
+            <%= submit %>
+          </button>
+        </form>
+        <% } %>
+
+ 4. make new post and redirected to /
+ 
+        app.post("/api/posts", async (req, res) => {
+        try {
+            const response = await axios.post(`${API_URL}/posts`, req.body);
+            console.log(response.data);
+            res.redirect("/");
+        } catch (error) {
+            res.status(500).json({ message: "Error creating post" });
+        }
+        });
+
+5. / will render to main home page that is index.ejs
+
+        app.get("/", async (req, res) => {
+        try {
+            const response = await axios.get(`${API_URL}/posts`);
+            console.log(response);
+            res.render("index.ejs", { posts: response.data });
+        } catch (error) {
+            res.status(500).json({ message: "Error fetching posts" });
+        }
+        });
---
+++ server.js
@@ -0,0 +1,84 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "http://localhost:4000"; //serverwhere the api requests are handled 
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Route to render the main page
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}/posts`);
+    console.log(response);
+    res.render("index.ejs", { posts: response.data });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching posts" });
+  }
+});
+
+// Route to render the modify.ejs page and go in else part  over there
+app.get("/new", (req, res) => {
+  res.render("modify.ejs", { heading: "New Post", submit: "Create Post" });
+});
+
+
+// PATCH a post when you just want to update one parameter ( first goto modify.ejs .. give all changes data and then call patch from there i.e below one which says partially patch)
+app.get("/edit/:id", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
+    console.log(response.data);
+    res.render("modify.ejs", {
+      heading: "Edit Post",
+      submit: "Update Post",
+      post: response.data,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching post" });
+  }
+});
+
+// Create a new post
+app.post("/api/posts", async (req, res) => {
+  try {
+    const response = await axios.post(`${API_URL}/posts`, req.body);
+    console.log(response.data);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error creating post" });
+  }
+});
+
+// Partially update a post
+app.post("/api/posts/:id", async (req, res) => {
+  console.log("called");
+  try {
+    const response = await axios.patch(
+      `${API_URL}/posts/${req.params.id}`,
+      req.body
+    );
+    console.log(response.data);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error updating post" });
+  }
+});
+
+// Delete a post
+app.get("/api/posts/delete/:id", async (req, res) => {
+  try {
+    await axios.delete(`${API_URL}/posts/${req.params.id}`);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting post" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Backend server is running on http://localhost:${port}`);
+});
